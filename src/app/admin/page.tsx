fb8d5535f8bb7985bce97c9b2779919e4123e8fe
Rauf Erk <erk.rauf@gmail.com>
2025-04-29 00:13:57 +0300

    Create base pages /admin, /live, /archive. Fix Tailwind text color

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,11 @@
+// src/app/admin/page.tsx
+
+export default function AdminPage() {
+  return (
+    <div className='flex min-h-screen items-center justify-center bg-gray-100 text-black'>
+      <h1 className='text-3xl font-bold'>
+        Страница админки (Управление трансляцией)
+      </h1>
+    </div>
+  )
+}
---
+++ src/app/archive/page.tsx
@@ -0,0 +1,9 @@
+// src/app/archive/page.tsx
+
+export default function ArchivePage() {
+  return (
+    <div className='flex min-h-screen items-center justify-center bg-gray-100 text-black'>
+      <h1 className='text-3xl font-bold'>Страница архива записей</h1>
+    </div>
+  )
+}
