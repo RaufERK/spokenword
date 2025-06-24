630d3dcfda4d6dc94eb5084bb4d5bf0a9faacdde
Rauf Erk <erk.rauf@gmail.com>
2025-06-24 15:32:44 +0300

    Насраиваем деплой 2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -17,7 +17,6 @@ module.exports = {
       host: '89.111.172.86',
       ref: 'origin/master',
       repo: 'git@github.com:RaufERK/spokenword.git',
-      // ecosystem.config.cjs  (фрагмент)
       postDeploy:
         'export PATH=$HOME/.nvm/versions/node/v22.15.1/bin:$PATH && pnpm install && pnpm prisma generate && pnpm prisma migrate deploy && pnpm build && pm2 reload ecosystem.config.cjs --only www.spokenword.ru',
       ssh_options: 'StrictHostKeyChecking=no',
